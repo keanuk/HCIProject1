3aa0759934ed7048db264950c2fc3f45456de001
Keanu Kerr <keanu@localhost.localdomain>
2018-02-04 15:45:33 -0500

    Attempted to fix API request

--- Code/script.js
+++ Code/script.js
@@ -1,73 +1,86 @@
 'use strict';
 
-let https = require ('https');
+var https = require ('https');
+var MsTranslator = require('mstranslator');
 
-// **********************************************
-// *** Update or verify the following values. ***
-// **********************************************
+// let subscriptionKey = 'bd1bde314ac94a7abe3c2a49af9969d4';
 
-// Replace the subscriptionKey string value with your valid subscription key.
-let subscriptionKey = 'bd1bde314ac94a7abe3c2a49af9969d4';
+var client = new MsTranslator({
+  api_key: "bd1bde314ac94a7abe3c2a49af9969d4"
+}, true);
 
-let host = 'api.microsofttranslator.com';
-let transPath = '/V2/Http.svc/Translate';
-let langPath = '/V2/Http.svc/GetLanguageNames';
-
-let target = 'fr-fr';
-let text = 'Hello';
-
-let params = '?to=' + target + '&text=' + encodeURI(text);
-
-
-console.log("Test");
-
-let response_handler = function (response) {
-    console.log("response");
-    let body = '';
-    response.on ('data', function (d) {
-        body += d;
-    });
-    response.on ('end', function () {
-        console.log (body);
-    });
-    response.on ('error', function (e) {
-        console.log ('Error: ' + e.message);
-    });
+var params = {
+  text: 'How\'s it going?'
+  , from: 'en'
+  , to: 'de'
 };
 
-
-let getLangList = function() {
-    let request_params = {
-        method : 'GET',
-        hostname : host,
-        path : langPath,
-        headers : {
-            'Ocp-Apim-Subscription-Key' : subscriptionKey,
-        }
-    };
-
-    let req = https.request (request_params, response_handler);
-    console.log("req");
-    console.log(req);
-    req.end ();
-
-}
-
-getLangList();
+client.translate(params, function(err, data) {
+  console.log(data);
+});
 
 
+//
+// let host = 'api.microsofttranslator.com';
+// let transPath = '/V2/Http.svc/Translate';
+// let langPath = '/V2/Http.svc/GetLanguageNames';
+//
+// let target = 'fr-fr';
+// let text = 'Hello';
+//
+// let params = '?to=' + target + '&text=' + encodeURI(text);
+//
+//
+// console.log("Test");
+//
+// let response_handler = function (response) {
+//     console.log("response");
+//     let body = '';
+//     response.on ('data', function (d) {
+//         body += d;
+//     });
+//     response.on ('end', function () {
+//         console.log (body);
+//     });
+//     response.on ('error', function (e) {
+//         console.log ('Error: ' + e.message);
+//     });
+// };
+//
+//
+// let getLangList = function() {
+//     let request_params = {
+//         method : 'GET',
+//         hostname : host,
+//         path : langPath,
+//         headers : {
+//             'Ocp-Apim-Subscription-Key' : subscriptionKey,
+//             'Content-Type':'application/xml',
+//         }
+//     };
+//
+//     let req = https.request (request_params, response_handler);
+//     console.log("req");
+//     req.end ();
+//
+// }
+//
+// getLangList();
+//
+//
 // let Translate = function () {
 //     let request_params = {
 //         method : 'GET',
 //         hostname : host,
-//         path : path + params,
+//         path : transPath + params,
 //         headers : {
 //             'Ocp-Apim-Subscription-Key' : subscriptionKey,
+//             'Content-Type':'application/xml',
 //         }
 //     };
-
+//
 //     let req = https.request (request_params, response_handler);
 //     req.end ();
 // }
-
+//
 // Translate ();
--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 
-// app.get('/', (req, res) => res.send('Hello World!'))
 app.use(express.static('Code'))
 
-app.listen(3000, () => console.log('Listening on port 3000')) 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'))
