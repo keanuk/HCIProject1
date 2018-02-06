647873c0d830023a60bdf3641cdb49e5be79392c
Keanu Kerr <keanukerr@gmail.com>
2018-02-05 23:51:06 -0500

    Now using correct API

--- Code/script.js
+++ Code/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var https = require ('https');
-var MsTranslator = require('mstranslator');
+// var MsTranslator = require('mstranslator');
 
 
 $(document).ready(function () {
@@ -23,67 +23,3 @@ $(document).ready(function () {
 });
 
 
-let subscriptionKey = 'bd1bde314ac94a7abe3c2a49af9969d4';
-
-let host = 'api.microsofttranslator.com';
-let transPath = '/V2/Http.svc/Translate';
-let langPath = '/V2/Http.svc/GetLanguageNames';
-
-let target = 'fr-fr';
-let text = 'Hello';
-
-let params = '?to=' + target + '&text=' + encodeURI(text);
-
-
-console.log("Test");
-
-let response_handler = function (response) {
-    console.log("response");
-    let body = '';
-    response.on ('data', function (d) {
-        body += d;
-    });
-    response.on ('end', function () {
-        console.log (body);
-    });
-    response.on ('error', function (e) {
-        console.log ('Error: ' + e.message);
-    });
-};
-
-
-let getLangList = function() {
-    let request_params = {
-        method : 'GET',
-        hostname : host,
-        path : langPath,
-        headers : {
-            'Ocp-Apim-Subscription-Key' : subscriptionKey,
-            'Content-Type':'application/xml',
-        }
-    };
-
-    let req = https.request (request_params, response_handler);
-    console.log("req");
-    req.end ();
-
-}
-
-getLangList();
-
-let Translate = function () {
-    let request_params = {
-        method : 'GET',
-        hostname : host,
-        path : transPath + params,
-        headers : {
-            'Ocp-Apim-Subscription-Key' : subscriptionKey,
-            'Content-Type':'application/xml',
-        }
-    };
-
-    let req = https.request (request_params, response_handler);
-    req.end ();
-}
-
-Translate ();
--- app.js
+++ app.js
@@ -1,14 +1,82 @@
+// var cors = require('cors')
+
 const express = require('express')
 const app = express()
 
 app.use(express.static('Code'))
-
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+// app.use(cors())
 
 app.listen(3000, () => console.log('Listening on port 3000'))
+
+var https = require ('https');
+
+// app.use(function (req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
+
+let subscriptionKey = '7ecce5e6c7a8422d9b97e8043903c36b';
+
+let host = 'api.microsofttranslator.com';
+let transPath = '/V2/Http.svc/Translate';
+let langPath = '/V2/Http.svc/GetLanguageNames';
+
+let target = 'fr-fr';
+let text = 'Hello';
+
+let params = '?to=' + target + '&text=' + encodeURI(text);
+
+
+console.log("Test");
+
+let response_handler = function (response) {
+    console.log("response");
+    let body = '';
+    response.on ('data', function (d) {
+        body += d;
+    });
+    response.on ('end', function () {
+        console.log (body);
+    });
+    response.on ('error', function (e) {
+        console.log ('Error: ' + e.message);
+    });
+};
+
+
+let getLangList = function() {
+    let request_params = {
+        method : 'GET',
+        hostname : host,
+        path : langPath,
+        headers : {
+            'Ocp-Apim-Subscription-Key' : subscriptionKey,
+        }
+    };
+
+    let req = https.request (request_params, response_handler);
+    console.log("req");
+    req.end ();
+
+}
+
+getLangList();
+
+let Translate = function () {
+    let request_params = {
+        method : 'GET',
+        hostname : host,
+        path : transPath + params,
+        headers : {
+            'Ocp-Apim-Subscription-Key' : subscriptionKey,
+        }
+    };
+
+    let req = https.request (request_params, response_handler);
+    req.end ();
+}
+
+Translate ();
