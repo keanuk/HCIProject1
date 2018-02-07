1d17fa14fe10326d4f1c053d9a211c78e4225390
Keanu Kerr <keanu@localhost.localdomain>
2018-02-07 08:13:00 -0500

    Works

--- Code/bundle.js
+++ Code/bundle.js
@@ -7583,11 +7583,41 @@ function extend() {
 }
 
 },{}],38:[function(require,module,exports){
-'use strict';
+// 'use strict';
+
+var socket = io.connect('http://localhost:3000');
 
 var https = require ('https');
 // var MsTranslator = require('mstranslator');
 
+console.log("Client works");
+
+var lang1 = document.getElementById('lang1');
+var lang2 = document.getElementById('lang2');
+var dirIn = document.getElementById('dirIn');
+
+console.log(lang2.options[lang2.selectedIndex].value);
+
+socket.on('newLang', function(data) {
+  console.log("Reveived Language");
+  console.log(data);
+  document.getElementById('lang1').insertAdjacentHTML( 'afterbegin', '<option>' + data + '</option>' );
+  document.getElementById('lang2').insertAdjacentHTML( 'afterbegin', '<option>' + data + '</option>' );
+});
+
+ document.getElementById("directionIn").addEventListener("click", function() {
+   console.log("Sending text");
+   socket.emit('getTranlation', {
+     dirIn: dirIn.value,
+     Lang: lang2.options[lang2.selectedIndex].value
+   });
+ });
+
+ socket.on('getTranlation', function(data) {
+   console.log("Reveived Tranlation");
+   console.log(data);
+   document.getElementById('dirList').insertAdjacentHTML( 'afterbegin', '<li>' + data + '</li>' );
+ });
 
 $(document).ready(function () {
   $('.langContinue').click(function() {
@@ -7600,17 +7630,11 @@ $(document).ready(function () {
     $('.destContinue').css('display', 'none');
   });
 
-  $('.addButton').click(function() {
-    var val = $('#dirIn').val();
-    $('ul.dirList').append('<li>' + val + '</li>');
-    // e.preventDefault();
-  });
-
-  $('#dirForm').submit(function(e){
-    var val = $(this).find('.dirIn').val();
-    $('ul.dirList').append('<li>' + val + '</li>');
-    e.preventDefault();
-  });
+  // $('.addButton').click(function() {
+  //   var val = $('#dirIn').val();
+  //   $('ul.dirList').append('<li>' + val + '</li>');
+  //   // e.preventDefault();
+  // });
 });
 
 },{"https":7}]},{},[38]);
--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 // import { parse } from 'url';
 
 const express = require('express')
-const app = express()
+// const app = express()
+
+var socket = require('socket.io');
+
+var app = express();
+var server = app.listen(3000, function() {
+  console.log("Listening on port 3000");
+});
 
 app.use(express.static('Code'))
 
-app.listen(3000, () => console.log('Listening on port 3000'))
+var io = socket(server);
+
+
+// app.listen(3000, () => console.log('Listening on port 3000'))
 
 var https = require ('https');
 
@@ -15,6 +25,8 @@ ejs.close = '}}';
 
 var parseString = require('xml2js').parseString;
 var convert = require('xml-js');
+
+var MsTranslator = require('mstranslator');
 
 let subscriptionKey = '7ecce5e6c7a8422d9b97e8043903c36b';
 
@@ -30,25 +42,18 @@ let params = '?to=' + target + '&text=' + encodeURI(text);
 
 var langArr = [];
 
-let response_handler = function (response) {
-    console.log("response");
-    let body = '';
-    response.on ('data', function (d) {
-        console.log("data");
-        console.log(d);
-        body += d;
-    });
-    response.on ('end', function () {
-        console.log("end");
-        console.log (body);
-    });
-    response.on ('error', function (e) {
-        console.log ('Error: ' + e.message);
-    });
-};
+var langNameArr = [];
 
-let arr_response_handler = function (response) {
-  console.log("arr response");
+var newTrans = 'help';
+
+
+var client = new MsTranslator({
+  api_key: "7ecce5e6c7a8422d9b97e8043903c36b"
+}, true);
+
+
+let lang_response_handler = function (response) {
+  console.log("Getting language names");
   let body = '';
   response.on ('data', function (d) {
       console.log("data");
@@ -57,9 +62,10 @@ let arr_response_handler = function (response) {
   });
   response.on ('end', function () {
       console.log("end");
-      // console.log (JSON.stringify(body));
+      console.log(body);
       parseString(body, function(err, result) {
-        console.log(result);
+        console.log(result)
+        // langNameArr = result;
       });
     });
   response.on ('error', function (e) {
@@ -67,6 +73,44 @@ let arr_response_handler = function (response) {
   });
 };
 
+
+let getLangNames = function() {
+  console.log("Getting language names");
+    let request_params = {
+        method : 'POST',
+        hostname : host,
+        path : langNamePath,
+        headers : {
+            'Ocp-Apim-Subscription-Key' : subscriptionKey,
+        }
+    };
+
+    let req = https.request (request_params, lang_response_handler);
+    req.end ();
+}
+
+
+let arr_response_handler = function (response) {
+  let body = '';
+  response.on ('data', function (d) {
+      body += d;
+  });
+  response.on ('end', function () {
+      parseString(body, function(err, result) {
+        console.log(result.ArrayOfstring.string)
+        langArr = result.ArrayOfstring.string;
+        // for(var i = 0; 3; i++) {
+        //   console.log(langArr[i]);
+        //   socket.emit('newLang', {
+        //     lang: langArr[i]
+        //   });
+        // }
+      });
+    });
+  response.on ('error', function (e) {
+      console.log ('Error: ' + e.message);
+  });
+};
 
 
 let getLangList = function() {
@@ -84,40 +128,64 @@ let getLangList = function() {
     req.end ();
 }
 
-let getLangNames = function() {
-  console.log("Getting language names");
-    let request_params = {
-        method : 'POST',
-        hostname : host,
-        path : langNamePath,
-        headers : {
-            'Ocp-Apim-Subscription-Key' : subscriptionKey,
-        }
-    };
-
-    let req = https.request (request_params, arr_response_handler);
-    req.end ();
-}
-
-
-let Translate = function () {
-  console.log("Translating");
-    let request_params = {
-        method : 'GET',
-        hostname : host,
-        path : transPath + params,
-        headers : {
-            'Ocp-Apim-Subscription-Key' : subscriptionKey,
-        }
-    };
-
-    let req = https.request (request_params, response_handler);
-    req.end ();
-}
-
-
 getLangList();
 
-getLangNames();
 
-Translate ();
+//
+// getLangNames();
+//
+// Translate ();
+
+io.on('connection', function(socket) {
+
+
+  socket.on('getTranlation', function(data) {
+
+    let response_handler = function (response) {
+        let body = '';
+        response.on ('data', function (d) {
+            console.log(d);
+            body += d;
+        });
+        response.on ('end', function () {
+            console.log (body);
+            // newTrans = body;
+            parseString(body, function(err, result) {
+              console.log(result.string._);
+              responseTrans = result.string._;
+              // socket.on('tranlated', function(responseTrans) {
+              // });
+              io.sockets.emit('getTranlation', result.string._);
+            });
+        });
+        response.on ('error', function (e) {
+            console.log ('Error: ' + e.message);
+        });
+    };
+
+    let Translate = function (parameters) {
+      console.log("Translating");
+        let request_params = {
+            method : 'GET',
+            hostname : host,
+            path : transPath + parameters,
+            headers : {
+                'Ocp-Apim-Subscription-Key' : subscriptionKey,
+            }
+        };
+
+        let req = https.request (request_params, response_handler);
+        req.end ();
+    }
+
+
+    console.log("recieved message");
+
+    console.log(data.dirIn);
+    var responseTrans = '';
+    newTarget = data.Lang;
+    var parameters = '?to=' + newTarget + '&text=' + encodeURI(data.dirIn);
+    Translate(parameters);
+
+  })
+});
